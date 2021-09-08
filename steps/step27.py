070da81e42ff2fdd4660c8c0ac54c12b9e223b24
Gyeong-Geun LEE <gglee4ai@gmail.com>
2021-09-08 10:48:44 +0900

    step28

--- steps/step27.py
+++ steps/step27.py
@@ -1,6 +1,6 @@
 if '__file__' in globals():
-  import os, sys
-  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import numpy as np
 from dezero import Function
