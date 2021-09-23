cf53cd928520c2b0ab91d70208838894abca698b
Gyeong-Geun LEE <gglee4ai@gmail.com>
2021-09-23 15:42:32 +0900

    step46

--- dezero/models.py
+++ dezero/models.py
@@ -27,5 +27,5 @@ class MLP(Model):
     def forward(self, x):
         for l in self.layers[:-1]:
             x = self.activation(l(x))
-            return self.layer[-1](x)
+            return self.layers[-1](x)
 
